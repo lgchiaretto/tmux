e618ee2bd040c3f7112dfee2403b5652ae6bfb5b
Luiz Gustavo Chiaretto <luizgustavo@chiaretto.com.br>
2024-11-03 00:40:39 -0300

    not verbose on tar

--- configure-local.sh
+++ configure-local.sh
@@ -13,7 +13,7 @@ echo Downloading tmux
 wget -q --no-check-certificate 'https://docs.google.com/uc?export=download&id=15gI-MP8HyaJ65WIH-F3-TSl_pOQuPjEB' -O tmux
 echo Downloading oc client
 wget -q https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.17.3/openshift-client-linux.tar.gz
-tar xzvf openshift-client-linux.tar.gz
+tar xzf openshift-client-linux.tar.gz
 echo Copying files to /usr/local/bin 
 sudo cp {tmux,oc} /usr/local/bin/
 echo "Done"
