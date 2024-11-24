5da88345a0a95102d8b91547834d43b714ba29d9
Luiz Gustavo Chiaretto <luizgustavo@chiaretto.com.br>
2024-11-24 03:14:42 -0300

    changed ocp client version

--- configure-local.sh
+++ configure-local.sh
@@ -12,13 +12,12 @@ chown -R $USER:$GROUP /home/$USER/.tmux/
 echo Downloading tmux
 wget -q --no-check-certificate 'https://docs.google.com/uc?export=download&id=15gI-MP8HyaJ65WIH-F3-TSl_pOQuPjEB' -O tmux
 echo Downloading oc client
-wget -q https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.17.3/openshift-client-linux.tar.gz
+wget -q https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.16.21/openshift-client-linux.tar.gz
 tar xzf openshift-client-linux.tar.gz
 echo Copying files to /usr/local/bin 
-#sudo cp {tmux,oc} /usr/local/bin/
-sudo cp tmux /usr/local/bin/
+sudo cp {tmux,oc} /usr/local/bin/
 sudo chmod +x /usr/local/bin/tmux
-#sudo chmod +x /usr/local/bin/oc
+sudo chmod +x /usr/local/bin/oc
 echo "Installing tmuxp"
 sudo dnf install -y python3-pip -q
 pip3 install tmuxp -q
