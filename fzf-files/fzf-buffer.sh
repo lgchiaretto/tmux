50a02b81d68a6542aa8786a5a5e300b389967913
Luiz Gustavo Chiaretto <luizgustavo@chiaretto.com.br>
2025-04-19 17:35:11 -0300

    new prefix + b

---
+++ fzf-files/fzf-buffer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+buffer=$(tmux list-buffers -F "#{buffer_name}: #{buffer_sample}" | fzf-tmux --wrap --reverse --height=40 -p80%,50% --preview "echo {} | fold -s -w 100" | awk -F': ' '{print $2}')
+
+
+# bind b display-popup  -w 80% -h 50% -E "sh -c '
+# tmux list-buffers -F \"#{buffer_sample}\" | fzf --wrap --reverse --height=100% --border=none --preview \"echo {} | fold -s -w 100\" |
+# xargs -r -I {} sh -c \"tmux paste-buffer -b {} && echo {} | fold -s -w 100\"
+# '"
+
+# Check if a buffer was selected
+if [ -n "$buffer" ]; then
+  tmux send-keys $(echo "$buffer" | tail -n1 | awk '{print $1}')
+fi
