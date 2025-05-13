b8c49f957351580ae71ca6530e265ca08db4dda0
Luiz Gustavo Chiaretto <luizgustavo@chiaretto.com.br>
2025-05-13 14:11:49 -0300

    added clusters available on manage cluster

--- fzf-files/fzf-manageclusters.sh
+++ fzf-files/fzf-manageclusters.sh
@@ -2,8 +2,31 @@
 
 actions="1 - Create cluster\n2 - Destroy cluster\n3 - Edit install configs\n4 - Export 'kube:admin' kubeconfig\n5 - Login with 'kubeadmin' user\n6 - Start cluster\n7 - Stop cluster\n8 - List OpenShift releases available on quay.chiaret.to"
 
+clusters() {
+  local dir="$1"
+  vmwarenotes=$(jq -r '.vmwarenotes' "/vms/clusters/$dir/$dir.json" 2>/dev/null || echo "No notes")
+  ocpversion=$(jq -r '.ocpversion' "/vms/clusters/$dir/$dir.json" 2>/dev/null || echo "No notes")
+  started_file="/vms/clusters/$dir/started"
+
+  if [ -f "$started_file" ]; then
+    printf "%-30s %-20s %-50s\n" "$dir *" "$ocpversion" "$vmwarenotes"
+  else
+    printf "%-30s %-20s %-50s\n" "$dir" "$ocpversion" "$vmwarenotes"
+  fi
+}
+
+selection_list=$(echo '----------------------------------------------------------'; echo; echo 'Cluster Name                   OCP Version          Description' ; echo; find /vms/clusters/ -mindepth 1 -maxdepth 1 -type d -name '*-*' \
+  ! -name 'backup-20230903' \
+  ! -name '*-files' \
+  ! -name 'quay-*' \
+  -exec basename {} \; | while read -r dir; do
+    clusters "$dir"
+  done)
+
+all_options="$actions\n\n$selection_list"
+
 selected_action=$(
-    echo -e "$actions" | fzf-tmux \
+    echo -e "$all_options" | fzf-tmux \
         --header=$'-------------------------- Help --------------------------
 [Enter]     Select the action
 [Esc]       Exit
@@ -42,8 +65,23 @@ if [ "$action" ]; then
         "3 - Edit install configs")
             /usr/local/bin/ocpvariablesfiles
             ;;
-        "8 - List OpenShift releases available on quay.chiaret.to")/
+        "8 - List OpenShift releases available on quay.chiaret.to")
             /usr/local/bin/ocpreleasesonquay
+            ;;
+        "Available Clusters:")
+            echo "You selected the clusters section."
+            ;;
+        Cluster:*)
+            cluster_name=$(echo "$action" | awk '{print $2}')
+            echo "You selected the cluster: $cluster_name"
+            # Here you can add specific actions for a selected cluster
+            ;;
+        *)
+            # If the selection does not match an action or the clusters header,
+            # we can consider it a cluster name directly.
+            # However, the formatting of the 'clusters()' output includes "Cluster:",
+            # so the case above should cover the selection of a cluster.
+            echo "Unknown option: $action"
             ;;
     esac
 fi
