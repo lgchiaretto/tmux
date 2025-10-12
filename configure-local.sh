e6ef0346845a67861b4816032f4dd0855a2915ba
Luiz Gustavo Chiaretto <luizgustavo@chiaretto.com.br>
2025-10-12 10:00:24 -0300

    Update OpenShift client version in script

--- configure-local.sh
+++ configure-local.sh
@@ -13,8 +13,8 @@ chown -R $USER:$GROUP /home/$USER/.tmux/
 echo Downloading tmux
 wget -q --no-check-certificate 'https://gpte-public-documents.s3.us-east-1.amazonaws.com/rh1_2025_lab17/rh1-lab17-tmux-binary' -O tmux
 echo Downloading oc client
-wget -q https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.19.12/openshift-client-linux-amd64-rhel9.tar.gz
-tar xzf openshift-client-linux.tar.gz
+wget -q https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.19.14/openshift-client-linux-amd64-rhel9.tar.gz
+tar xzf openshift-client-linux-amd64-rhel9.tar.gz
 echo Copying files to /usr/local/bin 
 sudo cp {tmux,oc} /usr/local/bin/
 sudo chmod +x /usr/local/bin/tmux
