86091180998860dfb9b8f626d792ca04c2f6f08e
Luiz Gustavo Chiaretto <luizgustavo@chiaretto.com.br>
2025-11-07 20:43:57 -0300

    fix

--- presentation/slides/08-resource-browsers.sh
+++ presentation/slides/08-resource-browsers.sh
@@ -34,12 +34,6 @@ echo -e "    │      \033[38;5;214mCtrl-d: Describe cluster operator\033[38;5;2
 echo -e "    │      \033[38;5;214mCtrl-e: Edit cluster operator\033[38;5;223m                                        │"
 echo -e "    │                                                                           │"
 echo -e "    │                                                                           │"
-echo -e "    │  \033[38;5;142mMulti-Host SSH Synchronization\033[38;5;223m                                           │"
-echo -e "    │                                                                           │"
-echo -e "    │      \033[38;5;214mtmux-sync-ssh host1 host2 host3   # 3 synchronized panes\033[38;5;223m             │"
-echo -e "    │      \033[38;5;214mtmux-sync-ssh hostname 4           # 4 panes to same host\033[38;5;223m            │"
-echo -e "    │      \033[38;5;214mtmux-sync-ossh master-0 master-1   # oc debug pods\033[38;5;223m                   │"
-echo -e "    │                                                                           │"
 echo -e "    └───────────────────────────────────────────────────────────────────────────┘"
 } | center_content
 
